8fde64690131b5c4baf4b440195f974691793211
le-chartreux <le-chartreux-vert@protonmail.com>
2023-04-23 23:02:46 +0200

    [dev tools] adding vue-typescript checking on vite dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/e-culture/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    checker({
+      vueTsc: true
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
