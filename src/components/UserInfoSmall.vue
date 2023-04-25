43479e5153c78509a57664bc1deaa77fa54d73d7
le-chartreux <le-chartreux-vert@protonmail.com>
2023-04-25 23:13:51 +0200

    [ui] adding name display on user info

--- src/components/UserInfoSmall.vue
+++ src/components/UserInfoSmall.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts">
-
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  props: {
+    userName: String
+  }
+})
 </script>
 
 <template>
-  <p>User info here</p>
+  <p v-if="userName">{{ userName }}</p>
+  <p v-else>User name not set</p>
 </template>
