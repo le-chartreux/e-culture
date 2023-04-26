cdfe812dd8126cfd13714fb5b29801c52ecfe32f
le-chartreux <le-chartreux-vert@protonmail.com>
2023-04-26 12:57:11 +0200

    [code style] cleaning

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,9 @@ import './assets/main.css'
 
 createApp(App).use(router).mount('#app')
 
-let initialRoute = '/'
 // to save current page when reloading
 window.addEventListener('beforeunload', () => {
   sessionStorage.setItem('lastRoute', router.currentRoute.value.path)
 })
-initialRoute = sessionStorage.getItem('lastRoute') || initialRoute
+const initialRoute = sessionStorage.getItem('lastRoute') || '/'
 router.push(initialRoute)
