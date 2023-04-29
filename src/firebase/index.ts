d657fc5ea5cced4a47dc0194c8424876b18b4c1a
le-chartreux <le-chartreux-vert@protonmail.com>
2023-04-29 22:22:50 +0200

    setup of firebase

---
+++ src/firebase/index.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { collection, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdrW-op61lrviPqesC6ez5K5aSxHf_Y14",
+  authDomain: "e-culture-71815.firebaseapp.com",
+  projectId: "e-culture-71815",
+  storageBucket: "e-culture-71815.appspot.com",
+  messagingSenderId: "52761179686",
+  appId: "1:52761179686:web:19db1bff3af727288658e7"
+};
+
+const firebaseApp = initializeApp(firebaseConfig)
+const db = getFirestore()
+const gamesRef = collection(db, "games")
+const playersRef = collection(db, "players")
+const quizzesRef = collection(db, "quizzes")
+
+export {firebaseApp, gamesRef, playersRef, quizzesRef}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import router from './router'
+import { firebaseApp } from "@/firebase";
+import { VueFire, VueFireFirestoreOptionsAPI } from "vuefire";
 
+import App from './App.vue'
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(
+  VueFire,
+  {
+    firebaseApp,
+    modules: [
+      VueFireFirestoreOptionsAPI()
+    ]
+  }
+)
+app.mount('#app')
 
 // to save current page when reloading
 window.addEventListener('beforeunload', () => {
@@ -12,3 +25,5 @@ window.addEventListener('beforeunload', () => {
 })
 const initialRoute = sessionStorage.getItem('lastRoute') || '/'
 router.push(initialRoute)
+  .then(() => { console.log(`location set to ${initialRoute}.`) })
+  .catch((reason) => {console.error(`impossible to set location to ${initialRoute} because ${reason}`)})
