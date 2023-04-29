9f36d04a0fad9f075ca93543908f8fa4c704d60f
le-chartreux <le-chartreux-vert@protonmail.com>
2023-04-29 22:25:32 +0200

    [ui/game] showing the list of players in the game

---
+++ src/components/PlayersList.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import type {PropType} from "vue";
+import type { Player } from "@/firebase/entities/Player";
+
+export default defineComponent({
+  props: {
+    players: Object as PropType<Player[]>
+  }
+})
+</script>
+
+<template>
+  <div id="player-list">
+    <ul>
+      <li v-for="player in players" :key="player.id">{{ player.pseudo }}</li>
+    </ul>
+  </div>
+
+</template>
--- src/views/GameVue.vue
+++ src/views/GameVue.vue
@@ -1,14 +1,29 @@
 <script lang="ts">
-import HeaderDefault from '@/components/Header/HeaderDefault.vue'
 import { defineComponent } from 'vue'
+import { gamesRef } from "@/firebase";
+import { doc } from "firebase/firestore";
+
+import HeaderDefault from '@/components/Header/HeaderDefault.vue'
+import PlayersList from "@/components/PlayersList.vue";
+import type { Game } from "@/firebase/entities/Game";
+
 
 export default defineComponent({
   data() {
     return {
-      gameId: this.$route.params.id
+      gameId: this.$route.params.id as string,
+      game: Object as unknown as Game
     }
   },
-  components: {HeaderDefault}
+  components: {HeaderDefault, PlayersList},
+  watch: {
+    gameId: {
+      immediate: true,
+      handler(gameId: string) {
+        this.$firestoreBind("game", doc(gamesRef, gameId))
+      }
+    }
+  }
 })
 </script>
 
@@ -18,6 +33,8 @@ export default defineComponent({
     <div id="game-information">
       <h2>Game {{gameId}}</h2>
       <p>Waiting for the game to start...</p>
+      <p>Players in this game:</p>
+      <PlayersList :players="game.players"></PlayersList>
     </div>
   </div>
 </template>
