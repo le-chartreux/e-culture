ae752ef0d39c6d4b02fca58f75b9b5fed9950cbc
le-chartreux <le-chartreux-vert@protonmail.com>
2023-05-01 01:36:40 +0200

    function to generate deterministic game from a string seed

--- src/firebase/entities/Game.ts
+++ src/firebase/entities/Game.ts
@@ -12,10 +12,22 @@ export class Game {
     return Quiz.ALL.length
   }
 
-  /* Create a deterministic game from a seed and a number of quizzes
+  /* Create a deterministic game from a string seed and a number of quizzes
    * numberOfQuizzes should be lower than the number of available quizzes!
    * */
-  static fromSeed(seed: number, numberOfQuizzes: number): Game {
+  static fromStringSeed(seed: string, numberOfQuizzes: number): Game {
+    return this.fromNumberSeed(this.stringSeedToNumberSeed(seed), numberOfQuizzes)
+  }
+
+  /* Sum the char codes of the string seed  */
+  private static stringSeedToNumberSeed(seed: string): number {
+    return seed.split('').reduce((partialSum, a) => partialSum + a.charCodeAt(0), 0);
+  }
+
+  /* Create a deterministic game from a number seed and a number of quizzes
+   * numberOfQuizzes should be lower than the number of available quizzes!
+   * */
+  static fromNumberSeed(seed: number, numberOfQuizzes: number): Game {
     const rng = prand.xoroshiro128plus(seed)
     const chosenQuizzes: Quiz[] = []
     const availableQuizzes = Quiz.ALL
