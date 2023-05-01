b699c52985bf09fc0b585b36236685f68afb676e
le-chartreux <le-chartreux-vert@protonmail.com>
2023-05-01 18:54:05 +0200

    adding the logic of GameRoom startTime and owner

--- src/firebase/entities/GameRoom.ts
+++ src/firebase/entities/GameRoom.ts
@@ -2,14 +2,27 @@ import { doc, DocumentReference, DocumentSnapshot, getDoc } from 'firebase/fires
 
 import { Player } from '@/firebase/entities/Player'
 import { gameRoomsRef } from '@/firebase'
+import { Game } from "@/firebase/entities/Game";
 
 export class GameRoom {
   id: string
   players: Player[]
+  startTime: Date | null
+  game: Game
 
-  constructor(id: string, players: Player[]) {
+  constructor(id: string, players: Player[], startTime: Date | null) {
     this.id = id
     this.players = players
+    this.startTime = startTime
+    this.game = Game.fromStringSeed(this.id, 10)
+  }
+
+  get owner(): Player | null {
+    return this.players[0] || null
+  }
+
+  isOwner(player: Player): boolean {
+    return player.equals(this.owner)
   }
 
   static async loadServerFromId(id: string): Promise<GameRoom> {
@@ -24,12 +37,13 @@ export class GameRoom {
   static async loadServerFromSnapshot(snapshot: DocumentSnapshot): Promise<GameRoom> {
     const playersPromises: Promise<Player>[] = []
     const playersRefs = snapshot.data()?.players
-    if (playersRefs) {
+    const startTime = snapshot.data()?.startTime || null
+    if (playersRefs instanceof Array) {
       for (const playerRef of playersRefs) {
         playersPromises.push(Player.loadServerFromRef(playerRef))
       }
       const players: Player[] = await Promise.all(playersPromises)
-      return new GameRoom(snapshot.id, players)
+      return new GameRoom(snapshot.id, players, startTime)
     } else {
       throw Error(`Impossible to get GameRoom from snapshot ${snapshot}: no data.`)
     }
@@ -41,5 +55,9 @@ export class GameRoom {
 
   static getRef(id: string): DocumentReference {
     return doc(gameRoomsRef, id)
+  }
+
+  get started(): boolean {
+    return this.startTime !== null
   }
 }
