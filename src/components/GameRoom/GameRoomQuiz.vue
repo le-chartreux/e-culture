fd7fff080564fe1bd73ee3268910f59d7c863e05
le-chartreux <le-chartreux-vert@protonmail.com>
2023-05-01 19:11:07 +0200

    [ui] component where the questions are asked

---
+++ src/components/GameRoom/GameRoomQuiz.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+import { defineComponent, type PropType } from "vue";
+import { Player } from "@/firebase/entities/Player";
+import type { GameRoom } from "@/firebase/entities/GameRoom";
+
+export default defineComponent({
+  data() {
+    return {
+      player: Player.loadLocal(),
+    }
+  },
+  props: {
+    gameRoom: {
+      type: Object as PropType<GameRoom>,
+      required: true
+    }
+  }
+})
+</script>
+
+<template>
+  <p>questions:</p>
+  <ul>
+    <li v-for="quiz in gameRoom.game.quizzes" :key="quiz.id">{{quiz.question}}</li>
+  </ul>
+</template>
