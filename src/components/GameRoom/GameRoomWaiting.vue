726df7f65a879ff46f9f1d97ba54d23478aa5aa1
le-chartreux <le-chartreux-vert@protonmail.com>
2023-05-09 21:24:45 +0200

    [ui] responsive game room waiting

--- src/components/GameRoom/GameRoomWaiting.vue
+++ src/components/GameRoom/GameRoomWaiting.vue
@@ -22,9 +22,19 @@ export default defineComponent({
 </script>
 
 <template>
-  <PlayersList :players="gameRoom.players" :owner="gameRoom.owner"></PlayersList>
-  <button class="pseudo-button" v-if="gameRoom.isOwner(player)" @click="gameRoom.startGameServer">
-    Start game
-  </button>
-  <p v-else>Waiting for the owner to start the game...</p>
+  <div id="game-room-waiting">
+    <PlayersList :players="gameRoom.players" :owner="gameRoom.owner"></PlayersList>
+    <button class="pseudo-button" v-if="gameRoom.isOwner(player)" @click="gameRoom.startGameServer">
+      Start game
+    </button>
+    <p v-else>Waiting for the owner to start the game...</p>
+  </div>
 </template>
+
+<style scoped>
+#game-room-waiting {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
