59e6347729bc1f2782bba318e7399ac09356abc5
le-chartreux <le-chartreux-vert@protonmail.com>
2023-05-09 21:25:07 +0200

    [code quality] reformat

--- src/components/GameRoom/GameRoomWaiting.vue
+++ src/components/GameRoom/GameRoomWaiting.vue
@@ -33,8 +33,8 @@ export default defineComponent({
 
 <style scoped>
 #game-room-waiting {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
