e9beb8c5846d6cd68ade9fbc7f667044f19e0dc9
le-chartreux <le-chartreux-vert@protonmail.com>
2023-05-09 17:41:18 +0200

    [ui] create game room works

---
+++ src/components/CreateGameRoom/CreateGameRoomInput.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import { GameRoom } from "@/firebase/entities/GameRoom";
+
+export default defineComponent({
+  data() {return {
+    gameRoomId: null as string | null
+  }},
+  methods: {
+    createGameRoom() {
+      if (this.gameRoomId) {
+        GameRoom.createGameRoom(this.gameRoomId)
+      } else {
+        throw Error(`Impossible to create a Game Room with gameRoomId ${this.gameRoomId}.`)
+      }
+    }
+  }
+});
+</script>
+
+<template>
+  <div id="create-game-route-input">
+    <input type="text" v-model="gameRoomId" placeholder="Game Room ID" />
+    <div v-if="gameRoomId">
+      <router-link
+        :to="'/game-room/' + gameRoomId"
+        class="pseudo-button"
+        @click="createGameRoom"
+      >Create Game Room '{{ gameRoomId }}'</router-link
+      >
+    </div>
+    <div v-else>
+      <p>Please enter a Game Room ID</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#create-game-route-input {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+}
+
+#create-game-route-input > * {
+    margin: 10px;
+    padding: 5px;
+}
+
+#create-game-route-input > input {
+    text-align: center;
+}
+
+</style>
--- src/views/CreateGameRoomView.vue
+++ src/views/CreateGameRoomView.vue
@@ -1,13 +1,36 @@
 <script lang="ts">
-import HeaderDefault from '@/components/Header/HeaderDefault.vue'
 import { defineComponent } from 'vue'
 
+import HeaderDefault from '@/components/Header/HeaderDefault.vue'
+import CreateGameRoomInput from "@/components/CreateGameRoom/CreateGameRoomInput.vue";
+
 export default defineComponent({
-  components: { HeaderDefault }
+  components: { HeaderDefault, CreateGameRoomInput }
 })
 </script>
 
 <template>
-  <HeaderDefault />
-  <p>Create Game Room here</p>
+  <div id="create-game-room">
+    <HeaderDefault />
+    <main>
+      <h2>Create a Game Room</h2>
+      <CreateGameRoomInput/>
+    </main>
+  </div>
 </template>
+
+<style scoped>
+#create-game-room {
+    display: flex;
+    flex-direction: column;
+    height: 100dvh;
+    padding: 1rem;
+}
+
+main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+</style>
