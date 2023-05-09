3eea9803407f3a2054e989b444661a9324e68c96
Felween <ademski@rocketmail.com>
2023-05-09 11:44:40 +0200

    [ui] test about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -14,18 +14,20 @@ export default defineComponent({
     <main>
       <img src="/logo/application.png" alt="application logo" />
       <h1>About e-culture</h1>
-      <p>E-culture is an educational multiplayer website hosting games meant to help people to learn English in a fun and dynamic way. 
-      
-      It is designed for anyone who is eager to learn more about the English language and culture. While it does not require an excellent proficiency in English, players do need to understand the questions in order to play correctly. 
-      
-      The current version of e-culture allows players to create or join a Game Room, as well as test each other’s knowledge with questions.</p>
+      <p>
+        E-culture is an educational multiplayer website hosting games meant to help people to learn
+        English in a fun and dynamic way. It is designed for anyone who is eager to learn more about
+        the English language and culture. While it does not require an excellent proficiency in
+        English, players do need to understand the questions in order to play correctly. The current
+        version of e-culture allows players to create or join a Game Room, as well as test each
+        other’s knowledge with questions.
+      </p>
     </main>
 
     <footer>
       <img src="/logo/company.png" alt="company logo" />
     </footer>
   </div>
-  
 </template>
 
 <style scoped>
